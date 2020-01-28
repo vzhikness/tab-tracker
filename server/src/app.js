35a906c7548ba0a436ac091f2ea82a2ee392a2c8
Anton Vakulenko <vakulenko.anton@gmail.com>
2020-01-28 23:23:48 +0300

    Set the registration page

--- server/src/app.js
+++ server/src/app.js
@@ -7,9 +7,9 @@ app.use(morgan('combined'))
 app.use(express.json())
 app.use(cors())
 
-app.get('/status', (req, res) => {
+app.post('/register', (req, res) => {
   res.send({
-    message: 'hello world'
+    message: `Hello ${req.body.email}! your user was registered! Have fun!`
   })
 })
 
